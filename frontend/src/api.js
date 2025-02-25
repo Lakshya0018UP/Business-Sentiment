7371e22bde43499d1838b14ca941b14c34e0c9a2
Lakshya Garg <lakshya.garg2004@gmail.com>
2025-02-25 19:30:30 +0530

    CORS

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:5000/api";
+const API_BASE_URL = "https://youtube-sentiment-xeg3.onrender.com/api/data";
 
 export const fetchComments = async (videoId) => {
     return axios.post(`${API_BASE_URL}/fetch-comments`, { video_id: videoId });
